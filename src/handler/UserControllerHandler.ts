3678a0760ecff302a334edd74dd981a28b45adac
Oliver Lehmann <lehmann@ans-netz.de>
2022-11-19 11:00:11 +0100

    Use Pinia for storing User-Session-Data.

--- src/handler/UserControllerHandler.ts
+++ src/handler/UserControllerHandler.ts
@@ -1,12 +1,17 @@
 import AbstractControllerHandler from "@/handler/AbstractControllerHandler";
 import { LoginRequest } from "@/model/rest/LoginRequest";
 import type { LoginResponse } from "@/model/rest/LoginResponse";
+import {
+  useUserSessionStore,
+  type UserSession,
+} from "@/stores/UserSessionStore";
 
 class UserControllerHandler extends AbstractControllerHandler {
   private static CONTROLLER = "user";
 
   async login(username: string, password: string): Promise<boolean> {
     const loginRequest = new LoginRequest(username, password);
+    const userSessionStore = useUserSessionStore();
 
     const response = await super.post(
       loginRequest,
@@ -23,10 +28,20 @@ class UserControllerHandler extends AbstractControllerHandler {
     }
 
     const loginResponse = (await response.json()) as LoginResponse;
+    const innerLoginResponse = loginResponse.loginResponse;
+    const userTransport = innerLoginResponse.userTransport;
 
-    console.log(loginResponse.loginResponse.token);
+    const userSession: UserSession = {
+      userId: userTransport.id,
+      userName: userTransport.userName,
+      userCanLogin: userTransport.userCanLogin === 1 ? true : false,
+      userIsNew: userTransport.userIsNew === 1 ? true : false,
+      userIsAdmin: userTransport.userIsAdmin === 1 ? true : false,
+      userAuthorizationToken: innerLoginResponse.token,
+    };
 
-    //TODO: Store in pinia
+    userSessionStore.setUserSession(userSession);
+
     return true;
   }
 }
