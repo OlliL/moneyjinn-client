1bce96cc9606cb291936ae569d0fb3d2b8fe86a3
Oliver Lehmann <lehmann@ans-netz.de>
2022-11-25 00:16:31 +0100

    performance boost by caching formatted dates, 50ms -> 2.3ms

--- src/tools/views/FormatDate.ts
+++ src/tools/views/FormatDate.ts
@@ -1,23 +1,28 @@
+const dateFormatter = new Intl.DateTimeFormat("default", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+});
+
+const timeFormatter = new Intl.DateTimeFormat("default", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
+const dateCache = new Map<Date, string>();
+
 export function formatDateWithTime(date: Date): string {
   //avoid comma between date and time
-  const dateStr = new Intl.DateTimeFormat("default", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  }).format(date);
-  const timeStr = new Intl.DateTimeFormat("default", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  }).format(date);
+  const dateStr = formatDate(date);
+  const timeStr = timeFormatter.format(date);
   return dateStr + " " + timeStr;
 }
 
 export function formatDate(date: Date): string {
-  const dateStr = new Intl.DateTimeFormat("default", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  }).format(date);
+  let dateStr = dateCache.get(date);
+  if (dateStr === undefined) {
+    dateStr = dateFormatter.format(date);
+    dateCache.set(date, dateStr);
+  }
   return dateStr;
 }
