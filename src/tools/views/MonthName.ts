bacbbf82dbc8712780d22438802f24be8f84a1d3
Oliver Lehmann <lehmann@ans-netz.de>
2022-11-25 00:27:15 +0100

    remove comment

--- src/tools/views/MonthName.ts
+++ src/tools/views/MonthName.ts
@@ -1,5 +1,5 @@
 export function getMonthName(month: number): string {
-  const date = new Date(2000, month - 1, 1); // 2009-11-10
+  const date = new Date(2000, month - 1, 1);
   const monthName = date.toLocaleString("default", { month: "long" });
   return monthName;
 }
