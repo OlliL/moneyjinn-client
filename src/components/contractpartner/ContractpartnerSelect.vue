6b3262f2cb26bc2840654b4169f14e35637f34bb
Oliver Lehmann <lehmann@ans-netz.de>
2022-11-25 14:49:20 +0100

    begin extracting the select boxes to reusable components

---
+++ src/components/contractpartner/ContractpartnerSelect.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="input-group">
+    <div class="form-floating">
+      <select
+        v-model="contractpartnerId"
+        id="contractpartner"
+        @change="emitContractpartnerSelected"
+        :class="'form-select form-control ' + inputClass"
+      >
+        <option value="0">&nbsp;</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+
+      <label for="contractpartner" :style="'color: ' + fieldColor">{{
+        fieldLabel
+      }}</label>
+    </div>
+    <span class="input-group-text"><i class="bi bi-plus"></i></span>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ContractpartnerSelect",
+  data() {
+    return { contractpartnerId: 0 };
+  },
+  emits: ["contractpartnerSelected"],
+  props: {
+    validityDate: {
+      type: Date,
+      required: true,
+    },
+    selectedId: {
+      type: Number,
+      required: false,
+    },
+    inputClass: {
+      type: String,
+      required: true,
+    },
+    fieldColor: {
+      type: String,
+      required: true,
+    },
+    fieldLabel: {
+      type: String,
+      required: true,
+    },
+  },
+  watch: {
+    selectedId: function (newVal: number, oldVal: number) {
+      if (newVal != oldVal) this.contractpartnerId = newVal;
+    },
+  },
+  methods: {
+    emitContractpartnerSelected() {
+      this.$emit("contractpartnerSelected", +this.contractpartnerId);
+    },
+  },
+});
+</script>
