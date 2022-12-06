38d5295ecc59d32b59f8389b41a0c98b06a272e3
Oliver Lehmann <lehmann@ans-netz.de>
2022-12-06 19:45:36 +0100

    progress

--- src/views/monthlysettlement/ListMonthlySettlements.vue
+++ src/views/monthlysettlement/ListMonthlySettlements.vue
@@ -37,7 +37,7 @@
                     <li class="page-item" v-for="month in months" :key="month">
                       <a
                         :class="
-                          $props.month == month + ''
+                          $props.month === month + ''
                             ? 'page-link active'
                             : 'page-link'
                         "
@@ -54,7 +54,28 @@
         </form>
       </div>
     </div>
-    <!-- conten vue here -->
+    <!---->
+    <div class="row justify-content-md-center" v-if="selectedMonth">
+      <div class="col-md-4 col-xs-12">
+        <div class="card">
+          <div class="card-header text-center p-3">
+            <h5>Geldbewegung {{ monthName }} {{ year }}</h5>
+          </div>
+          <div class="card-body">
+            <table class="table table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>Kapitalquelle</th>
+                  <th>Betrag</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!---->
   </div>
 </template>
 
@@ -72,26 +93,41 @@ export default defineComponent({
       months: [] as number[],
       years: [] as number[],
       selectedYear: 0,
+      selectedMonth: 0,
     };
   },
   props: {
     year: {
       type: String,
-      required: true,
+      default: undefined,
     },
     month: {
       type: String,
-      required: true,
+      default: undefined,
     },
   },
   created() {
-    this.loadData(this.$props.year, this.$props.month);
+    const year: number | undefined = this.$props.year
+      ? +this.$props.year
+      : undefined;
+    const month: number | undefined = this.$props.month
+      ? +this.$props.month
+      : undefined;
+
+    this.loadMonth(year, month);
+    if (year !== undefined && month !== undefined)
+      this.loadMontlySettlements(year, month);
+  },
+  computed: {
+    monthName(): string {
+      return getMonthName(this.selectedMonth);
+    },
   },
   methods: {
     getMonthName(month: number): string {
       return getMonthName(month);
     },
-    async loadData(year?: string, month?: string) {
+    async loadMonth(year?: number, month?: number) {
       let response = await MonthlySettlementControllerHandler.getAvailableMonth(
         year,
         month
@@ -100,15 +136,20 @@ export default defineComponent({
       this.years = response.allYears;
 
       this.selectedYear = response.year;
-
+      this.selectedMonth = response.month;
+      console.log(response);
+      console.log(month);
       this.dataLoaded = true;
     },
+    loadMontlySettlements(year: number, month: number) {},
     selectMonth(year: string, month?: string) {
       router.push({
         name: Routes.ListMonthlySettlements,
         params: { year: year, month: month },
       });
-      if (this.$props.year != year) this.loadData(year, month);
+      if (this.$props.year + "" != year) this.loadMonth(+year);
+      if (month) this.selectedMonth = +month;
+      console.log(this.selectedMonth);
     },
     showCreateMonthlySettlementModal() {},
   },
