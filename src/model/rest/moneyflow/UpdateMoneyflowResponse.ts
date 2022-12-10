75db0360367e0ecc137035d391283f642ed1dd76
Oliver Lehmann <lehmann@ans-netz.de>
2022-12-10 02:02:12 +0100

    start working on search moneyflows

--- src/model/rest/moneyflow/UpdateMoneyflowResponse.ts
+++ src/model/rest/moneyflow/UpdateMoneyflowResponse.ts
@@ -1,8 +1,10 @@
+import type { ErrorResponse } from "../ErrorResponse";
 import type { MoneyflowSplitEntryTransport } from "../transport/MoneyflowSplitEntryTransport";
 import type { MoneyflowTransport } from "../transport/MoneyflowTransport";
 import type { ValidationItemTransport } from "../transport/ValidationItemTransport";
 
 export type UpdateMoneyflowResponse = {
+  error?: ErrorResponse;
   updateMoneyflowResponse: {
     result: boolean;
     validationItemTransport: Array<ValidationItemTransport>;
