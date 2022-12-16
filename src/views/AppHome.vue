0f3b8b9170efc850755cbc90f08ac63d706825f8
Oliver Lehmann <lehmann@ans-netz.de>
2022-12-16 15:11:02 +0100

    closes #15

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -3,7 +3,52 @@
     ref="editModal"
     @monthly-settlement-upserted="monthlySettlementUpserted"
   />
-  &nbsp;
+  <div class="container-fluid">
+    <div class="row justify-content-md-center text-center">
+      <div class="col-xs-12 mb-4">
+        <h4>Aufgabenliste</h4>
+      </div>
+    </div>
+    <div class="row justify-content-md-center">
+      <div class="col-sm-11 col-lg-9 col-xl-6 col-xxl-4 mb-4">
+        <ul
+          class="list-group"
+          v-if="importedMoneyflows || monthlySettlementMissing"
+        >
+          <li class="list-group-item" v-if="importedMoneyflows">
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="navigateImportMoneyflows()"
+            >
+              bearbeiten</button
+            ><span class="align-middle"
+              >&nbsp;Es liegen noch nicht bearbeitete importierte Geldbewegungen
+              vor!</span
+            >
+          </li>
+          <li class="list-group-item" v-if="monthlySettlementMissing">
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="showEditMonthlySettlementModal()"
+            >
+              bearbeiten</button
+            ><span class="align-middle"
+              >&nbsp;F&uuml;r den vergangenen Monat existiert kein
+              Monatsabschlu&szlig;!</span
+            >
+          </li>
+        </ul>
+        <div
+          class="text-center text-success"
+          v-if="!importedMoneyflows && !monthlySettlementMissing"
+        >
+          Alles erledigt! <i class="bi bi-emoji-smile"></i>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,9 +56,18 @@ import { defineComponent } from "vue";
 import EditMonthlySettlementModalVue from "@/components/monthlysettlement/EditMonthlySettlementModal.vue";
 import type { Events } from "@/model/event/Events";
 import EventControllerHandler from "@/handler/EventControllerHandler";
+import router, { Routes } from "@/router";
 
 export default defineComponent({
   name: "HomeView",
+  data() {
+    return {
+      importedMoneyflows: false,
+      monthlySettlementMissing: false,
+      monthlySettlementMonth: 0,
+      monthlySettlementYear: 0,
+    };
+  },
   mounted() {
     this.loadData();
   },
@@ -21,16 +75,28 @@ export default defineComponent({
     async loadData() {
       const events: Events = await EventControllerHandler.showEventList();
       console.log(events);
-      //FIXME: Display link to import Moneyflows if they exist
-      //FIXME: Display Modal-Link for Monthly Settlement-Creation if needed
+
+      if (events.numberOfImportedMoneyflows > 0) {
+        this.importedMoneyflows = true;
+      }
+      this.monthlySettlementMissing = events.monthlySettlementMissing;
+      this.monthlySettlementMonth = events.monthlySettlementMonth;
+      this.monthlySettlementYear = events.monthlySettlementYear;
     },
-    showEditMonthlySettlementModal(year?: number, month?: number) {
+    showEditMonthlySettlementModal() {
       (this.$refs.editModal as typeof EditMonthlySettlementModalVue)._show(
-        year,
-        month
+        this.monthlySettlementYear,
+        this.monthlySettlementMonth
       );
     },
-    monthlySettlementUpserted() {},
+    monthlySettlementUpserted() {
+      this.monthlySettlementMissing = false;
+    },
+    navigateImportMoneyflows() {
+      router.push({
+        name: Routes.ImportMoneyflows,
+      });
+    },
   },
   components: {
     EditMonthlySettlementModalVue,
