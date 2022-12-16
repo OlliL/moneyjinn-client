76640dbc9e05e8bbaf3736825f9139550c4aacc3
Oliver Lehmann <lehmann@ans-netz.de>
2022-12-16 13:36:52 +0100

    begin work on #15

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,8 +1,36 @@
-<template>&nbsp;</template>
+<template>
+  <EditMonthlySettlementModalVue
+    ref="editModal"
+    @monthly-settlement-upserted="monthlySettlementUpserted"
+  />
+  &nbsp;
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import EditMonthlySettlementModalVue from "@/components/monthlysettlement/EditMonthlySettlementModal.vue";
+
 export default defineComponent({
   name: "HomeView",
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      //FIXME: Call System Events Controller Endpoint
+      //FIXME: Display link to import Moneyflows if they exist
+      //FIXME: Display Modal-Link for Monthly Settlement-Creation if needed
+    },
+    showEditMonthlySettlementModal(year?: number, month?: number) {
+      (this.$refs.editModal as typeof EditMonthlySettlementModalVue)._show(
+        year,
+        month
+      );
+    },
+    monthlySettlementUpserted() {},
+  },
+  components: {
+    EditMonthlySettlementModalVue,
+  },
 });
 </script>
