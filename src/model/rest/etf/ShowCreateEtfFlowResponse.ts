f26162c0fe2a60f743d3c8d5118a5b2b7ce89213
Oliver Lehmann <lehmann@ans-netz.de>
2022-12-18 01:03:30 +0100

    remove not needed files

--- src/model/rest/etf/ShowCreateEtfFlowResponse.ts
+++
@@ -1,8 +0,0 @@
-import type { ErrorResponse } from "../ErrorResponse";
-import type { EtfTransport } from "../transport/EtfTransport";
-
-export type ShowCreateEtfFlowResponse = ErrorResponse & {
-  showCreateEtfFlowResponse: {
-    etfTransport: Array<EtfTransport>;
-  };
-};
