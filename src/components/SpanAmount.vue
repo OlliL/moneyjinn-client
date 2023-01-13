8d4944eb6b69aed217d9b4301a19981ce32ead1b
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-13 16:51:54 +0100

    introduce common materials for amount and date spans

---
+++ src/components/SpanAmount.vue
@@ -0,0 +1,23 @@
+<template>
+  <span :class="amountClass">{{ amountString }} &euro;</span>
+</template>
+
+<script lang="ts" setup>
+import { formatNumber, redIfNegative } from "@/tools/views/FormatNumber";
+import { computed } from "vue";
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    required: true,
+  },
+});
+
+const amountClass = computed(() => {
+  return redIfNegative(props.amount);
+});
+
+const amountString = computed(() => {
+  return formatNumber(props.amount, 2);
+});
+</script>
---
+++ src/components/SpanDate.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>{{ dateString }}</span>
+</template>
+
+<script lang="ts" setup>
+import { formatDate } from "@/tools/views/FormatDate";
+import { computed } from "vue";
+
+const props = defineProps({
+  date: {
+    type: Date,
+    required: false,
+  },
+});
+
+const dateString = computed(() => {
+  return props.date ? formatDate(props.date) : "";
+});
+</script>
