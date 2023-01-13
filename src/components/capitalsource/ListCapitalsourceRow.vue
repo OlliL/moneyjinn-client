56d341606e96716a2b9637f6249d7ff97c7578ed
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-13 20:04:18 +0100

    composition API

--- src/components/capitalsource/ListCapitalsourceRow.vue
+++ src/components/capitalsource/ListCapitalsourceRow.vue
@@ -5,8 +5,8 @@
     <td class="text-start">{{ stateString }}</td>
     <td class="text-start">{{ mcs.accountNumber }}</td>
     <td class="text-start">{{ mcs.bankCode }}</td>
-    <td class="text-start">{{ validFromString }}</td>
-    <td class="text-start">{{ validTilString }}</td>
+    <td class="text-start"><SpanDate :date="mcs.validFrom" /></td>
+    <td class="text-start"><SpanDate :date="mcs.validTil" /></td>
     <td class="text-center">
       <b :style="'color:' + groupUseColor">{{ groupUseString }}</b>
     </td>
@@ -25,60 +25,56 @@
     </td>
   </tr>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
+import { computed, type PropType } from "vue";
+
+import SpanDate from "../SpanDate.vue";
+
 import type { Capitalsource } from "@/model/capitalsource/Capitalsource";
 import { capitalsourceImportNames } from "@/model/capitalsource/CapitalsourceImport";
 import { capitalsourceStateNames } from "@/model/capitalsource/CapitalsourceState";
 import { capitalsourceTypeNames } from "@/model/capitalsource/CapitalsourceType";
-import { formatDate } from "@/tools/views/FormatDate";
-import { defineComponent, type PropType } from "vue";
 
-export default defineComponent({
-  name: "ListCapitalsourceRow",
-  props: {
-    mcs: {
-      type: Object as PropType<Capitalsource>,
-      required: true,
-    },
-    owner: {
-      type: Boolean,
-      required: true,
-    },
+const props = defineProps({
+  mcs: {
+    type: Object as PropType<Capitalsource>,
+    required: true,
   },
-  emits: ["deleteCapitalsource", "editCapitalsource"],
-  computed: {
-    groupUseColor(): string {
-      return this.mcs.groupUse ? "green" : "red";
-    },
-    groupUseString(): string {
-      return this.mcs.groupUse ? "Ja" : "Nein";
-    },
-    importAllowedColor(): string {
-      return this.mcs.importAllowed > 0 ? "green" : "red";
-    },
-    importAllowedString(): string {
-      return capitalsourceImportNames[this.mcs.importAllowed];
-    },
-    typeString(): string {
-      return capitalsourceTypeNames[this.mcs.type];
-    },
-    stateString(): string {
-      return capitalsourceStateNames[this.mcs.state];
-    },
-    validFromString(): string {
-      return formatDate(this.mcs.validFrom);
-    },
-    validTilString(): string {
-      return formatDate(this.mcs.validTil);
-    },
-  },
-  methods: {
-    deleteCapitalsource() {
-      this.$emit("deleteCapitalsource", this.mcs);
-    },
-    editCapitalsource() {
-      this.$emit("editCapitalsource", this.mcs);
-    },
+  owner: {
+    type: Boolean,
+    required: true,
   },
 });
+const emit = defineEmits(["deleteCapitalsource", "editCapitalsource"]);
+
+const groupUseColor = computed(() => {
+  return props.mcs.groupUse ? "green" : "red";
+});
+const groupUseString = computed(() => {
+  return props.mcs.groupUse ? "Ja" : "Nein";
+});
+
+const importAllowedColor = computed(() => {
+  return props.mcs.importAllowed > 0 ? "green" : "red";
+});
+
+const importAllowedString = computed(() => {
+  return capitalsourceImportNames[props.mcs.importAllowed];
+});
+
+const typeString = computed(() => {
+  return capitalsourceTypeNames[props.mcs.type];
+});
+
+const stateString = computed(() => {
+  return capitalsourceStateNames[props.mcs.type];
+});
+
+const deleteCapitalsource = () => {
+  emit("deleteCapitalsource", props.mcs);
+};
+
+const editCapitalsource = () => {
+  emit("editCapitalsource", props.mcs);
+};
 </script>
