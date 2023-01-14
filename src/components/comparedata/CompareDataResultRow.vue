bb4d91959d62ae7d29c95aef420f73c2c38f24fe
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-14 17:09:56 +0100

    Composition API

--- src/components/comparedata/CompareDataResultRow.vue
+++ src/components/comparedata/CompareDataResultRow.vue
@@ -1,9 +1,9 @@
 <template>
   <tr v-if="mmf">
     <th>Datenbank</th>
-    <td>{{ mmfBookingdateString }}</td>
-    <td>{{ mmfInvoicedateString }}</td>
-    <td :class="mmfAmountClass">{{ mmfAmountString }} &euro;</td>
+    <td><SpanDate :date="mmf.bookingDate" /></td>
+    <td><SpanDate :date="mmf.invoiceDate" /></td>
+    <td class="text-end"><SpanAmount :amount="mmf.amount" /></td>
     <td class="text-start">{{ mmf.contractpartnerName }}</td>
     <td class="text-start">{{ mmf.comment }}</td>
     <td class="text-start">
@@ -23,9 +23,15 @@
   </tr>
   <tr v-if="importData">
     <th>Quelle</th>
-    <td>{{ importDataBookingdateString }}</td>
-    <td>{{ importDataInvoicedateString }}</td>
-    <td :class="mmfAmountClass">{{ importDataAmountString }} &euro;</td>
+    <td>
+      <SpanDate :date="importData.bookingDate" />
+    </td>
+    <td>
+      <SpanDate :date="importData.invoiceDate" />
+    </td>
+    <td class="text-end">
+      <SpanAmount :amount="importData.amount" />
+    </td>
     <td class="text-start">{{ importData.partner }}</td>
     <td class="text-start">{{ importData.comment }}</td>
     <td class="text-start">
@@ -35,72 +41,43 @@
   </tr>
   <tr style="border-bottom: 2px solid #000"></tr>
 </template>
-<script lang="ts">
-import type { Moneyflow } from "@/model/moneyflow/Moneyflow";
-import { defineComponent, type PropType } from "vue";
-import { formatDate } from "@/tools/views/FormatDate";
-import { redIfNegativeEnd, formatNumber } from "@/tools/views/FormatNumber";
-import { useUserSessionStore } from "@/stores/UserSessionStore";
-import type { CompareDataDataset } from "@/model/comparedata/CompareDataDataset";
+<script lang="ts" setup>
+import { computed, type PropType } from "vue";
 
-export default defineComponent({
-  name: "CompareDataResultRow",
-  props: {
-    mmf: {
-      type: Object as PropType<Moneyflow>,
-      required: false,
-    },
-    importData: {
-      type: Object as PropType<CompareDataDataset>,
-      required: false,
-    },
-    capitalsourceComment: {
-      type: String,
-      required: true,
-    },
+import SpanAmount from "../SpanAmount.vue";
+import SpanDate from "../SpanDate.vue";
+
+import { useUserSessionStore } from "@/stores/UserSessionStore";
+
+import type { CompareDataDataset } from "@/model/comparedata/CompareDataDataset";
+import type { Moneyflow } from "@/model/moneyflow/Moneyflow";
+
+const props = defineProps({
+  mmf: {
+    type: Object as PropType<Moneyflow>,
+    required: false,
   },
-  emits: ["deleteMoneyflow", "editMoneyflow"],
-  computed: {
-    mmfBookingdateString() {
-      return this.mmf ? formatDate(this.mmf.bookingDate) : "";
-    },
-    mmfInvoicedateString() {
-      return this.mmf && this.mmf.invoiceDate
-        ? formatDate(this.mmf.invoiceDate)
-        : "";
-    },
-    mmfAmountClass(): string {
-      return this.mmf ? redIfNegativeEnd(this.mmf.amount) : "";
-    },
-    mmfAmountString(): string {
-      return this.mmf ? formatNumber(this.mmf.amount, 2) : "";
-    },
-    isOwnMoneyflow(): boolean {
-      const userSessionStore = useUserSessionStore();
-      return this.mmf ? this.mmf.userId === userSessionStore.getUserId : false;
-    },
-    importDataBookingdateString() {
-      return this.importData ? formatDate(this.importData.bookingDate) : "";
-    },
-    importDataInvoicedateString() {
-      return this.importData && this.importData.invoiceDate
-        ? formatDate(this.importData.invoiceDate)
-        : "";
-    },
-    importDataAmountClass(): string {
-      return this.importData ? redIfNegativeEnd(this.importData.amount) : "";
-    },
-    importDataAmountString(): string {
-      return this.importData ? formatNumber(this.importData.amount, 2) : "";
-    },
+  importData: {
+    type: Object as PropType<CompareDataDataset>,
+    required: false,
   },
-  methods: {
-    deleteMoneyflow() {
-      if (this.mmf) this.$emit("deleteMoneyflow", this.mmf.id);
-    },
-    editMoneyflow() {
-      if (this.mmf) this.$emit("editMoneyflow", this.mmf.id);
-    },
+  capitalsourceComment: {
+    type: String,
+    required: true,
   },
 });
+
+const userSessionStore = useUserSessionStore();
+const emit = defineEmits(["deleteMoneyflow", "editMoneyflow"]);
+
+const isOwnMoneyflow = computed(() => {
+  return props.mmf ? props.mmf.userId === userSessionStore.getUserId : false;
+});
+
+const deleteMoneyflow = () => {
+  if (props.mmf) emit("deleteMoneyflow", props.mmf.id);
+};
+const editMoneyflow = () => {
+  if (props.mmf) emit("editMoneyflow", props.mmf.id);
+};
 </script>
