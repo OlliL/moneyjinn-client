fd46375c7d4017edd98d19841c5645b2bd1a1bd4
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-21 00:50:10 +0100

    Composition API #26

--- src/components/moneyflow/SearchMoneyflowResultRow.vue
+++ src/components/moneyflow/SearchMoneyflowResultRow.vue
@@ -1,7 +1,7 @@
 <template>
   <tr>
-    <td>{{ bookingdateString }}</td>
-    <td :class="amountClass">{{ amountString }} &euro;</td>
+    <td><SpanDate :date="mmf.bookingDate" /></td>
+    <td class="text-end"><SpanAmount :amount="mmf.amount" /></td>
     <td class="text-start">{{ mmf.contractpartnerName }}</td>
     <td class="text-start">{{ mmf.comment }}</td>
     <td class="text-start">{{ mmf.postingAccountName }}</td>
@@ -21,44 +21,33 @@
     <td colspan="2" v-if="!isOwnMoneyflow"></td>
   </tr>
 </template>
-<script lang="ts">
-import type { Moneyflow } from "@/model/moneyflow/Moneyflow";
-import { defineComponent, type PropType } from "vue";
-import { formatDate } from "@/tools/views/FormatDate";
-import { redIfNegativeEnd, formatNumber } from "@/tools/views/FormatNumber";
+<script lang="ts" setup>
+import { computed, type PropType } from "vue";
+
+import SpanAmount from "../SpanAmount.vue";
+import SpanDate from "../SpanDate.vue";
+
 import { useUserSessionStore } from "@/stores/UserSessionStore";
 
-export default defineComponent({
-  name: "SearchMoneyflowResultRow",
-  props: {
-    mmf: {
-      type: Object as PropType<Moneyflow>,
-      required: true,
-    },
-  },
-  emits: ["deleteMoneyflow", "editMoneyflow"],
-  computed: {
-    bookingdateString() {
-      return formatDate(this.mmf.bookingDate);
-    },
-    amountClass(): string {
-      return redIfNegativeEnd(this.mmf.amount);
-    },
-    amountString(): string {
-      return formatNumber(this.mmf.amount, 2);
-    },
-    isOwnMoneyflow(): boolean {
-      const userSessionStore = useUserSessionStore();
-      return this.mmf.userId === userSessionStore.getUserId;
-    },
-  },
-  methods: {
-    deleteMoneyflow() {
-      this.$emit("deleteMoneyflow", this.mmf.id);
-    },
-    editMoneyflow() {
-      this.$emit("editMoneyflow", this.mmf.id);
-    },
+import type { Moneyflow } from "@/model/moneyflow/Moneyflow";
+
+const props = defineProps({
+  mmf: {
+    type: Object as PropType<Moneyflow>,
+    required: true,
   },
 });
+const userSessionStore = useUserSessionStore();
+const emit = defineEmits(["deleteMoneyflow", "editMoneyflow"]);
+
+const isOwnMoneyflow = computed(() => {
+  return props.mmf.userId === userSessionStore.getUserId;
+});
+
+const deleteMoneyflow = () => {
+  emit("deleteMoneyflow", props.mmf.id);
+};
+const editMoneyflow = () => {
+  emit("editMoneyflow", props.mmf.id);
+};
 </script>
