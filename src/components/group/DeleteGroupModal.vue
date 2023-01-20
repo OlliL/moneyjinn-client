3492e10989f1821086b8decce9597290a2152796
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-20 20:20:21 +0100

    Composition API, zod, vee-validate for #26

--- src/components/group/DeleteGroupModal.vue
+++ src/components/group/DeleteGroupModal.vue
@@ -1,15 +1,7 @@
 <template>
   <ModalVue title="Benutzergruppe l&ouml;schen" ref="modalComponent">
     <template #body>
-      <div v-if="serverError">
-        <div
-          class="alert alert-danger"
-          v-for="(error, index) in serverError"
-          :key="index"
-        >
-          {{ error }}
-        </div>
-      </div>
+      <DivError :server-errors="serverErrors" />
       <div class="row">
         <div
           class="text-start d-flex align-items-center col-sm-3 col-xs-5"
@@ -28,51 +20,38 @@
   </ModalVue>
 </template>
 
-<script lang="ts">
-import GroupControllerHandler from "@/handler/GroupControllerHandler";
-import type { Group } from "@/model/group/Group";
-import type { ValidationResult } from "@/model/validation/ValidationResult";
-import { getError } from "@/tools/views/ThrowError";
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { ref } from "vue";
+
+import DivError from "../DivError.vue";
 import ModalVue from "../Modal.vue";
 
-export default defineComponent({
-  name: "DeleteGroupModal",
-  data() {
-    return {
-      group: {} as Group,
-      serverError: new Array<string>(),
-    };
-  },
-  computed: {},
-  methods: {
-    async _show(group: Group) {
-      this.group = group;
-      this.serverError = new Array<string>();
-      (this.$refs.modalComponent as typeof ModalVue)._show();
-    },
-    followUpServerCall(validationResult: ValidationResult): boolean {
-      if (!validationResult.result) {
-        this.serverError = new Array<string>();
-        for (let resultItem of validationResult.validationResultItems) {
-          this.serverError.push(getError(resultItem.error));
-        }
-        return false;
+import { handleServerError } from "@/tools/views/ThrowError";
+
+import type { Group } from "@/model/group/Group";
+
+import GroupControllerHandler from "@/handler/GroupControllerHandler";
+
+const group = ref({} as Group);
+const serverErrors = ref(new Array<string>());
+const modalComponent = ref();
+const emit = defineEmits(["groupDeleted"]);
+
+const _show = (_group: Group) => {
+  group.value = _group;
+  serverErrors.value = new Array<string>();
+  modalComponent.value._show();
+};
+
+const deleteGroup = () => {
+  GroupControllerHandler.deleteGroup(group.value.id).then(
+    (validationResult) => {
+      if (!handleServerError(validationResult, serverErrors)) {
+        modalComponent.value._hide();
+        emit("groupDeleted", group.value);
       }
-      return true;
-    },
-    async deleteGroup() {
-      const validationResult = await GroupControllerHandler.deleteGroup(
-        this.group.id
-      );
-      if (this.followUpServerCall(validationResult)) {
-        (this.$refs.modalComponent as typeof ModalVue)._hide();
-        this.$emit("groupDeleted", this.group);
-      }
-    },
-  },
-  expose: ["_show"],
-  emits: ["groupDeleted"],
-  components: { ModalVue },
-});
+    }
+  );
+};
+defineExpose({ _show });
 </script>
