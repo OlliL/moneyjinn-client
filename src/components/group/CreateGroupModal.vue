924b89b50efde88e7462ff04e9da9dffdabc2984
Oliver Lehmann <lehmann@ans-netz.de>
2023-01-20 20:20:21 +0100

    Composition API, zod, vee-validate for #26

--- src/components/group/CreateGroupModal.vue
+++ src/components/group/CreateGroupModal.vue
@@ -1,28 +1,17 @@
 <template>
   <ModalVue :title="title" ref="modalComponent">
     <template #body
-      ><form @submit.prevent="createGroup" :id="'createGroupForm' + idSuffix">
+      ><form @submit.prevent="createGroup" id="createGroupForm">
         <div class="container-fluid">
-          <div v-if="serverError">
-            <div
-              class="alert alert-danger"
-              v-for="(error, index) in serverError"
-              :key="index"
-            >
-              {{ error }}
-            </div>
-          </div>
+          <DivError :server-errors="serverErrors" />
           <div class="row pt-2">
             <div class="col-xs-12">
-              <div class="form-floating">
-                <input
-                  v-model="group.name"
-                  :id="'groupName' + idSuffix"
-                  type="text"
-                  class="form-control"
-                />
-                <label :for="'groupName' + idSuffix">Name</label>
-              </div>
+              <InputStandard
+                v-model="group.name"
+                :validation-schema="schema.name"
+                id="name"
+                field-label="Name"
+              />
             </div>
           </div>
         </div>
@@ -32,134 +21,84 @@
       <button type="button" class="btn btn-secondary" @click="resetForm">
         r&uuml;cksetzen
       </button>
-      <button
-        type="submit"
-        class="btn btn-primary"
-        :form="'createGroupForm' + idSuffix"
-      >
-        Speichern
-      </button>
+      <ButtonSubmit button-label="Speichern" form-id="createGroupForm" />
     </template>
   </ModalVue>
 </template>
 
-<script lang="ts">
-import type { Group } from "@/model/group/Group";
-import GroupControllerHandler from "@/handler/GroupControllerHandler";
-import { generateErrorData, type ErrorData } from "@/tools/views/ErrorData";
-import { getError } from "@/tools/views/ThrowError";
-import { validateInputField } from "@/tools/views/ValidateInputField";
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { useForm } from "vee-validate";
+import { computed, ref } from "vue";
+import { string, ZodType } from "zod";
+
+import ButtonSubmit from "../ButtonSubmit.vue";
+import DivError from "../DivError.vue";
 import ModalVue from "../Modal.vue";
-import type { ValidationResult } from "@/model/validation/ValidationResult";
 
-type CreateGroupModalData = {
-  group: Group;
-  origGroup: Group | undefined;
-  serverError: Array<String>;
-  nameIsValid: boolean | null;
-  nameErrorMessage: string;
+import { globErr } from "@/tools/views/ZodUtil";
+import { handleServerError } from "@/tools/views/ThrowError";
+
+import type { Group } from "@/model/group/Group";
+
+import GroupControllerHandler from "@/handler/GroupControllerHandler";
+import InputStandard from "../InputStandard.vue";
+
+const serverErrors = ref(new Array<string>());
+
+const schema: Partial<{ [key in keyof Group]: ZodType }> = {
+  name: string(globErr("Bitte Name angeben!")).min(1),
 };
-export default defineComponent({
-  name: "CreateGroupModal",
-  data(): CreateGroupModalData {
-    return {
-      group: {} as Group,
-      origGroup: undefined,
-      serverError: {} as Array<String>,
-      nameIsValid: null,
-      nameErrorMessage: "",
-    };
-  },
-  props: {
-    idSuffix: {
-      type: String,
-      default: "",
-    },
-  },
-  computed: {
-    formIsValid(): boolean {
-      const isValid = this.nameIsValid;
-      if (isValid === null || isValid === undefined || isValid === true) {
-        return true;
-      }
-      return false;
-    },
-    title(): string {
-      return this.origGroup === undefined
-        ? "Benutzergruppe hinzufügen"
-        : "Benutzergruppe bearbeiten";
-    },
-    nameErrorData(): ErrorData {
-      return generateErrorData(this.nameIsValid, "Name", this.nameErrorMessage);
-    },
-  },
-  methods: {
-    async _show(group?: Group) {
-      this.origGroup = group ? group : undefined;
-      this.resetForm();
-      (this.$refs.modalComponent as typeof ModalVue)._show();
-    },
-    resetForm() {
-      if (this.origGroup) {
-        this.group = JSON.parse(JSON.stringify(this.origGroup));
-      } else {
-        this.group = {} as Group;
-        this.group.name = "";
-      }
-      this.nameIsValid = null;
-      this.serverError = {} as Array<String>;
-    },
-    validateName() {
-      [this.nameIsValid, this.nameErrorMessage] = validateInputField(
-        this.group.name,
-        "Name angeben!"
-      );
-    },
-    handleServerError(validationResult: ValidationResult): boolean {
-      if (!validationResult.result) {
-        this.serverError = new Array<string>();
-        for (let resultItem of validationResult.validationResultItems) {
-          this.serverError.push(getError(resultItem.error));
-        }
-      }
-      return !validationResult.result;
-    },
-    async createGroup() {
-      this.validateName();
 
-      if (this.formIsValid) {
-        if (this.group.id > 0) {
-          //update
-          const validationResult = await GroupControllerHandler.updateGroup(
-            this.group
-          );
-          if (!this.handleServerError(validationResult)) {
-            (this.$refs.modalComponent as typeof ModalVue)._hide();
-            this.$emit("groupUpdated", this.group);
-          }
-        } else {
-          //create
-          const groupValidation = GroupControllerHandler.createGroup(
-            this.group
-          );
-          const validationResult = await (
-            await groupValidation
-          ).validationResult;
+const group = ref({} as Group);
+const origGroup = ref({} as Group | undefined);
+const modalComponent = ref();
+const emit = defineEmits(["groupUpdated", "groupCreated"]);
 
-          if (!this.handleServerError(validationResult)) {
-            this.group = (await groupValidation).group;
-            (this.$refs.modalComponent as typeof ModalVue)._hide();
-            this.$emit("groupCreated", this.group);
-          }
-        }
-      }
-    },
-  },
-  expose: ["_show"],
-  emits: ["groupCreated", "groupUpdated"],
-  components: {
-    ModalVue,
-  },
+const { handleSubmit, values, setFieldTouched } = useForm();
+
+const title = computed(() => {
+  return origGroup.value === undefined
+    ? "Gruppe hinzufügen"
+    : "Gruppe bearbeiten";
 });
+
+const resetForm = () => {
+  if (origGroup.value) {
+    Object.assign(group.value, origGroup.value);
+  } else {
+    group.value = {} as Group;
+  }
+  serverErrors.value = new Array<string>();
+  Object.keys(values).forEach((field) => setFieldTouched(field, false));
+};
+
+const _show = async (_group?: Group) => {
+  origGroup.value = _group ? _group : undefined;
+  resetForm();
+  modalComponent.value._show();
+};
+
+const createGroup = handleSubmit(() => {
+  if (group.value.id > 0) {
+    //update
+    GroupControllerHandler.updateGroup(group.value).then((validationResult) => {
+      if (!handleServerError(validationResult, serverErrors)) {
+        modalComponent.value._hide();
+        emit("groupUpdated", group.value);
+      }
+    });
+  } else {
+    //create
+    GroupControllerHandler.createGroup(group.value).then((groupValidation) => {
+      const validationResult = groupValidation.validationResult;
+
+      if (!handleServerError(validationResult, serverErrors)) {
+        group.value = groupValidation.group;
+        modalComponent.value._hide();
+        emit("groupCreated", group.value);
+      }
+    });
+  }
+});
+defineExpose({ _show });
 </script>
--- src/components/group/ListGroupRow.vue
+++ src/components/group/ListGroupRow.vue
@@ -13,27 +13,23 @@
     </td>
   </tr>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
+import type { PropType } from "vue";
 import type { Group } from "@/model/group/Group";
-import { defineComponent, type PropType } from "vue";
 
-export default defineComponent({
-  name: "ListGroupRow",
-  props: {
-    group: {
-      type: Object as PropType<Group>,
-      required: true,
-    },
-  },
-  emits: ["deleteGroup", "editGroup"],
-  computed: {},
-  methods: {
-    deleteGroup() {
-      this.$emit("deleteGroup", this.group);
-    },
-    editGroup() {
-      this.$emit("editGroup", this.group);
-    },
+const props = defineProps({
+  group: {
+    type: Object as PropType<Group>,
+    required: true,
   },
 });
+
+const emit = defineEmits(["deleteGroup", "editGroup"]);
+
+const deleteGroup = () => {
+  emit("deleteGroup", props.group);
+};
+const editGroup = () => {
+  emit("editGroup", props.group);
+};
 </script>
