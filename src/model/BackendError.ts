2f858d03bb0f412173fd7e08121f001091c50a11
Oliver Lehmann <lehmann@ans-netz.de>
2023-02-19 11:50:13 +0100

    Start reworking error handling #31

---
+++ src/model/BackendError.ts
@@ -0,0 +1,48 @@
+import { getError } from "@/tools/views/ThrowError";
+import type { ValidationResult } from "./validation/ValidationResult";
+
+export enum BackendErrorType {
+  CLIENT_ERROR,
+  AUTH_ERROR,
+  VALIDATION_ERROR,
+  ERROR,
+}
+
+export class BackendError {
+  private errorType: BackendErrorType;
+  private errorCode: number | undefined;
+  private errorMessage: string | undefined;
+  private validationResult: ValidationResult | undefined;
+
+  constructor(
+    _errorType: BackendErrorType,
+    _errorCode?: number,
+    _errorMessage?: string,
+    _validationResult?: ValidationResult
+  ) {
+    this.errorType = _errorType;
+    this.errorCode = _errorCode;
+    this.errorMessage = _errorMessage;
+    this.validationResult = _validationResult;
+  }
+
+  getErrorType(): BackendErrorType {
+    return this.errorType;
+  }
+
+  getErrorCode(): number | undefined {
+    return this.errorCode;
+  }
+
+  getErrorMessage(): string {
+    return this.errorMessage
+      ? this.errorMessage
+      : this.errorCode
+      ? getError(this.errorCode)
+      : "";
+  }
+
+  getValidationResult(): ValidationResult | undefined {
+    return this.validationResult;
+  }
+}
