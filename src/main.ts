2afbf62895dc4b2aa2f113a6b73c06a4c7e9d714
Oliver Lehmann <lehmann@ans-netz.de>
2023-02-25 12:36:41 +0100

    start with  I18n #12

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { PiniaSharedState } from "pinia-shared-state";
@@ -10,7 +11,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
 import "../node_modules/vanillajs-datepicker/dist/css/datepicker-bs5.min.css";
+
 import { AxiosInstanceHolder } from "./handler/AxiosInstanceHolder";
+
+import de from "./locales/de.json";
+import en from "./locales/en.json";
+
+const i18n = createI18n({
+  locale: navigator.language,
+  fallbackLocale: "en",
+  messages: { de, en },
+});
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -23,6 +34,7 @@ pinia.use(
 
 app.use(pinia);
 app.use(router);
+app.use(i18n);
 
 AxiosInstanceHolder.getInstance();
 
