7429d0e832b240954f71be773819388c82587355
Oliver Lehmann <lehmann@ans-netz.de>
2023-03-03 19:47:39 +0100

    I18n #12

--- src/tools/views/MonthName.ts
+++ src/tools/views/MonthName.ts
@@ -1,5 +1,5 @@
 export function getMonthName(month: number): string {
   const date = new Date(2000, month - 1, 1);
-  const monthName = date.toLocaleString("default", { month: "long" });
+  const monthName = date.toLocaleString(navigator.language, { month: "long" });
   return monthName;
 }
