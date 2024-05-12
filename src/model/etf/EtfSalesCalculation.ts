4240c1ce0cb8d7ad23f9eaaf6d5143e971171946
OlliL <lehmann@ans-netz.de>
2024-05-12 14:50:24 +0200

    support absolute and relative transaction costs

--- src/model/etf/EtfSalesCalculation.ts
+++ src/model/etf/EtfSalesCalculation.ts
@@ -6,7 +6,10 @@ export type EtfSalesCalculation = {
   profit: number;
   accumulatedPreliminaryLumpSum: number;
   chargeable: number;
-  transactionCosts: number;
+  transactionCostsAbsoluteSell: number;
+  transactionCostsAbsoluteBuy: number;
+  transactionCostsRelativeSell: number;
+  transactionCostsRelativeBuy: number;
   rebuyLosses: number;
   overallCosts: number;
   pieces: number;
