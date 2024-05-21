bf209b649deb46dfcac6030b30fb21b1a800c676
OlliL <lehmann@ans-netz.de>
2024-05-21 16:49:17 +0200

    fix mobile view

--- src/components/etf/ListEtfDepotSummary.vue
+++ src/components/etf/ListEtfDepotSummary.vue
@@ -2,37 +2,37 @@
   <div class="row justify-content-md-center" v-if="etfSummary">
     <div class="col-xxl-6 col-xs-12 mb-4">
       <table class="table table-striped table-bordered table-hover">
-        <col style="width: 10%" />
-        <col style="width: 12%" />
-        <col style="width: 12%" />
-        <col style="width: 12%" />
-        <col style="width: 12%" />
-        <col style="width: 12%" />
-        <col style="width: 12%" />
-        <col style="width: 18%" />
         <thead>
           <tr>
             <th class="text-center" rowspan="2" id="thShares">
               {{ $t("ETFFlow.shares") }}
             </th>
-            <th class="text-center" colspan="3" id="thAmount">
+            <th
+              class="text-center d-none d-md-table-cell"
+              colspan="3"
+              id="thAmount"
+            >
               {{ $t("ETFFlow.amount") }}
             </th>
             <th class="text-center" colspan="3" id="thOverall">
               {{ $t("ETFFlow.overall") }}
             </th>
-            <th class="text-center" rowspan="2" id="thState">
+            <th
+              class="text-center d-none d-md-table-cell"
+              rowspan="2"
+              id="thState"
+            >
               {{ $t("Reports.state") }}
             </th>
           </tr>
           <tr>
-            <th class="text-center" id="thAmountPayed">
+            <th class="text-center d-none d-md-table-cell" id="thAmountPayed">
               {{ $t("ETFFlow.payed") }} &#8709;
             </th>
-            <th class="text-center" id="thAmountBid">
+            <th class="text-center d-none d-md-table-cell" id="thAmountBid">
               {{ $t("ETFFlow.bid") }}
             </th>
-            <th class="text-center" id="thAmountAsk">
+            <th class="text-center d-none d-md-table-cell" id="thAmountAsk">
               {{ $t("ETFFlow.ask") }}
             </th>
             <th class="text-center" id="thOverallPayed">
