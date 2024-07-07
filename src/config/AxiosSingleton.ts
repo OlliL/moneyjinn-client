692d04457b71a15c31ef0e1d1ed415e118f9d8ef
OlliL <lehmann@ans-netz.de>
2024-07-07 17:03:06 +0200

    add missing error code

--- src/config/AxiosSingleton.ts
+++ src/config/AxiosSingleton.ts
@@ -22,78 +22,30 @@ export class AxiosSingleton {
 
   public static getInstance(): AxiosSingleton {
     if (!AxiosSingleton.instance) {
-      AxiosSingleton.instance = new AxiosSingleton();
+      const axiosSingleton = new AxiosSingleton();
 
-      AxiosSingleton.instance.axiosInstance = axios.create({
-        baseURL: AxiosSingleton.instance.getWebRoot(),
+      axiosSingleton.axiosInstance = axios.create({
+        baseURL: axiosSingleton.getWebRoot(),
         withCredentials: true,
       });
 
       const requestRefresh: TokenRefreshRequest = async (
         refreshToken: Token,
       ) => {
-        return AxiosSingleton.instance
+        return axiosSingleton
           .refreshAuthToken(refreshToken)
           .then((response) => {
             return response;
           });
       };
 
-      applyAuthTokenInterceptor(AxiosSingleton.instance.axiosInstance, {
+      applyAuthTokenInterceptor(axiosSingleton.axiosInstance, {
         requestRefresh,
       });
 
-      AxiosSingleton.instance.axiosInstance.interceptors.response.use(
-        (response) => {
-          return response;
-        },
-        (error) => {
-          if (error instanceof Error && !("response" in error)) {
-            throw new BackendError(
-              BackendErrorType.ERROR,
-              undefined,
-              I18nSingleton.t()("ErrorMessage.technicalError"),
-            );
-          }
+      applyErrorHandlerInterceptor(axiosSingleton.axiosInstance);
 
-          if (error.response.status === 400) {
-            const errorResponse: ErrorResponse = error.response.data;
-            throw new BackendError(
-              BackendErrorType.CLIENT_ERROR,
-              errorResponse.code,
-            );
-          } else if (error.response.status === 403) {
-            throw new BackendError(
-              BackendErrorType.AUTH_ERROR,
-              ErrorCode.USERNAME_PASSWORD_WRONG.valueOf(),
-            );
-          } else if (error.response.status === 422) {
-            const validationResponse: ValidationResponse = error.response.data;
-            const validationResult: ValidationResult = {
-              result: validationResponse.result,
-              validationResultItems:
-                validationResponse.validationItemTransports?.map((vit) => {
-                  return mapValidationItemTransportToModel(vit);
-                }),
-            };
-
-            throw new BackendError(
-              BackendErrorType.VALIDATION_ERROR,
-              undefined,
-              undefined,
-              validationResult,
-            );
-          }
-          throw new BackendError(
-            BackendErrorType.ERROR,
-            undefined,
-            I18nSingleton.t()("ErrorMessage.technicalError", {
-              status: error.response.status,
-              statusText: error.response.statusText,
-            }),
-          );
-        },
-      );
+      AxiosSingleton.instance = axiosSingleton;
     }
     return AxiosSingleton.instance;
   }
@@ -102,7 +54,7 @@ export class AxiosSingleton {
     return "http://" + webServerHost;
   }
 
-  public async refreshAuthToken(refreshToken: Token): Promise<IAuthTokens> {
+  private async refreshAuthToken(refreshToken: Token): Promise<IAuthTokens> {
     const headers = {} as Record<string, string>;
     headers["Authorization"] = "Bearer " + refreshToken;
 
@@ -127,4 +79,58 @@ export class AxiosSingleton {
   public getAxiosInstance(): AxiosInstance {
     return this.axiosInstance;
   }
+}
+
+function applyErrorHandlerInterceptor(axios: AxiosInstance): void {
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error instanceof Error && !("response" in error)) {
+        throw new BackendError(
+          BackendErrorType.ERROR,
+          undefined,
+          I18nSingleton.t()("ErrorMessage.technicalError"),
+        );
+      }
+
+      if (error.response.status === 400) {
+        const errorResponse: ErrorResponse = error.response.data;
+        throw new BackendError(
+          BackendErrorType.CLIENT_ERROR,
+          errorResponse.code,
+        );
+      } else if (error.response.status === 403) {
+        throw new BackendError(
+          BackendErrorType.AUTH_ERROR,
+          ErrorCode.USERNAME_PASSWORD_WRONG.valueOf(),
+        );
+      } else if (error.response.status === 422) {
+        const validationResponse: ValidationResponse = error.response.data;
+        const validationResult: ValidationResult = {
+          result: validationResponse.result,
+          validationResultItems:
+            validationResponse.validationItemTransports?.map((vit) => {
+              return mapValidationItemTransportToModel(vit);
+            }),
+        };
+
+        throw new BackendError(
+          BackendErrorType.VALIDATION_ERROR,
+          undefined,
+          undefined,
+          validationResult,
+        );
+      }
+      throw new BackendError(
+        BackendErrorType.ERROR,
+        undefined,
+        I18nSingleton.t()("ErrorMessage.technicalError", {
+          status: error.response.status,
+          statusText: error.response.statusText,
+        }),
+      );
+    },
+  );
 }
