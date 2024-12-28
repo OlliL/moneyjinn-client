9d1237e3be11452b187250abd04a29f426ba8915
OlliL <lehmann@ans-netz.de>
2024-12-28 16:10:49 +0100

    work around bug colinhacks/zod#3486

--- src/tools/views/ZodUtil.ts
+++ src/tools/views/ZodUtil.ts
@@ -8,10 +8,22 @@ export function globErr(message: string) {
   };
 }
 
+const regExpCache = new Map<number, RegExp>();
+
 export function amountSchema(
   message: string,
   decimalPlaces: number = 2,
 ): ZodType {
   const zodMessage = globErr(message);
-  return number(zodMessage).multipleOf(Math.pow(10, decimalPlaces * -1));
+
+  let pattern = regExpCache.get(decimalPlaces);
+  if (pattern === undefined) {
+    pattern = new RegExp("^\\d*\\.\\d{0," + decimalPlaces + "}$");
+    regExpCache.set(decimalPlaces, pattern);
+  }
+
+  return number(zodMessage).refine((num) => pattern.test("" + num));
+
+  // broken: https://github.com/colinhacks/zod/issues/3486
+  //multipleOf(Math.pow(10, decimalPlaces * -1));
 }
