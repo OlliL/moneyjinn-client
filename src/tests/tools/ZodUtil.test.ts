d1a19dcf9d2420dadd16ef345bee8561cf7c1501
OlliL <lehmann@ans-netz.de>
2024-12-28 16:58:32 +0100

    integers are supported as well

--- src/tests/tools/ZodUtil.test.ts
+++ src/tests/tools/ZodUtil.test.ts
@@ -5,9 +5,11 @@ const decimalPlacesDefaultSchema = amountSchema("error");
 const decimalPlaces8Schema = amountSchema("error", 8);
 
 test.each([
+  [decimalPlacesDefaultSchema, 2, true],
   [decimalPlacesDefaultSchema, 2.1, true],
   [decimalPlacesDefaultSchema, 2.12, true],
   [decimalPlacesDefaultSchema, 2.123, false],
+  [decimalPlaces8Schema, 2, true],
   [decimalPlaces8Schema, 2.1, true],
   [decimalPlaces8Schema, 2.12345678, true],
   [decimalPlaces8Schema, 2.123456789, false],
