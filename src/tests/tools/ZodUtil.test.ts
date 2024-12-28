c93e318a6e791b2290fb7e98d3eb67362ab79f2c
OlliL <lehmann@ans-netz.de>
2024-12-28 16:10:49 +0100

    work around bug colinhacks/zod#3486

--- src/tests/tools/ZodUtil.test.ts
+++ src/tests/tools/ZodUtil.test.ts
@@ -1,9 +1,17 @@
 import { amountSchema } from "@/tools/views/ZodUtil";
 import { expect, test } from "vitest";
 
-test("amount with default decimapPlaces", async () => {
-  const zodAmount = amountSchema("error");
-  const result = zodAmount.safeParse(2.45);
-  console.log(result);
-  expect(result.success).true;
+const decimalPlacesDefaultSchema = amountSchema("error");
+const decimalPlaces8Schema = amountSchema("error", 8);
+
+test.each([
+  [decimalPlacesDefaultSchema, 2.1, true],
+  [decimalPlacesDefaultSchema, 2.12, true],
+  [decimalPlacesDefaultSchema, 2.123, false],
+  [decimalPlaces8Schema, 2.1, true],
+  [decimalPlaces8Schema, 2.12345678, true],
+  [decimalPlaces8Schema, 2.123456789, false],
+])("amountSchema, decimalPlaces", async (schema, num, expectedResult) => {
+  const result = schema.safeParse(num);
+  expect(result.success).toBe(expectedResult);
 });
