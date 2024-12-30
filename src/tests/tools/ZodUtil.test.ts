165e7e38427a163ab23bcf6fbcfc55f483987557
OlliL <lehmann@ans-netz.de>
2024-12-30 15:49:52 +0100

    negative numbers are also allowed as amount of course

--- src/tests/tools/ZodUtil.test.ts
+++ src/tests/tools/ZodUtil.test.ts
@@ -13,6 +13,14 @@ test.each([
   [decimalPlaces8Schema, 2.1, true],
   [decimalPlaces8Schema, 2.12345678, true],
   [decimalPlaces8Schema, 2.123456789, false],
+  [decimalPlacesDefaultSchema, -2, true],
+  [decimalPlacesDefaultSchema, -2.1, true],
+  [decimalPlacesDefaultSchema, -2.12, true],
+  [decimalPlacesDefaultSchema, -2.123, false],
+  [decimalPlaces8Schema, -2, true],
+  [decimalPlaces8Schema, -2.1, true],
+  [decimalPlaces8Schema, -2.12345678, true],
+  [decimalPlaces8Schema, -2.123456789, false],
 ])("amountSchema, decimalPlaces", async (schema, num, expectedResult) => {
   const result = schema.safeParse(num);
   expect(result.success).toBe(expectedResult);
