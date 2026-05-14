91d504d07e6063c7df51010799959b2712635ec3
OlliL <lehmann@ans-netz.de>
2026-05-14 14:26:28 +0200

    remove test code

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -117,7 +117,6 @@ const loadData = () => {
         ? events.monthlySettlementYear
         : 0;
       dataLoaded.value = true;
-      monthlySettlementMissing.value = true;
     })
     .catch((backendError) => {
       handleBackendError(backendError, serverErrors);
