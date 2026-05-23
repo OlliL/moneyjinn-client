62a5a9e12fabc73e608bd3885c63835ad3183e4f
OlliL <lehmann@ans-netz.de>
2026-05-23 15:23:43 +0200

    type fixes

--- env.d.ts
+++ env.d.ts
@@ -1,16 +1,16 @@
 /// <reference types="vite/client" />
 
 // Allow importing CSS files as side-effect modules in TypeScript
-declare module '*.css';
-declare module '*.scss';
-declare module '*.sass';
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
 
 // If Vite types are not picked up for some toolchains, provide minimal ImportMeta typing
 interface ImportMetaEnv {
-	readonly BASE_URL?: string;
-	// add other env vars here as needed
+  readonly BASE_URL?: string;
+  // add other env vars here as needed
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
