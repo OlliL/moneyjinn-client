01991e4c0575d40b4e36bf6d8ce2039618cf1564
OlliL <lehmann@ans-netz.de>
2026-05-23 15:23:32 +0200

    type fixes

--- src/router/index.ts
+++ src/router/index.ts
@@ -38,7 +38,7 @@ export enum DropdownMenus {
   Profile = "profile",
 }
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory((import.meta as any).env?.BASE_URL),
   routes: [
     {
       path: "/login/:error?",
