beb9e73400b731eaec7a460c01bd864601376c30
OlliL <lehmann@ans-netz.de>
2026-06-06 13:22:54 +0200

    Toaster at bottom on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
 import GlobalModals from "@/components/common/GlobalModals.vue";
 import { Toaster } from "@/components/ui/sonner";
+import { isDesktop } from "@/tools/views/IsDesktop";
+import { computed } from "vue";
 import { RouterView } from "vue-router";
+
+const position = computed(() =>
+  isDesktop().value ? "top-center" : "bottom-center",
+);
 </script>
 
 <template>
   <router-view />
   <GlobalModals />
   <Toaster
-    position="top-center"
+    :position="position"
     rich-colors
     close-button
     :toastOptions="{
